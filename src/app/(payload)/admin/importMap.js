a4593f920fec529b443c5a617a3792889355ab11
sagarmusabbir <sagarmusabbir@gmail.com>
2026-01-07 03:13:03 +0600

    Fix migration for existing database objects

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -21,8 +21,8 @@ import { LinkToDoc as LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloa
 import { ReindexButton as ReindexButton_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloadcms/plugin-search/client'
 import { RowLabel as RowLabel_ec255a65fa6fa8d1faeb09cf35284224 } from '@/Header/RowLabel'
 import { RowLabel as RowLabel_1f6ff6ff633e3695d348f4f3c58f1466 } from '@/Footer/RowLabel'
-import { Icon as Icon_9c1a3ff8a9adb7b1e4b7a5cd42725bdb } from '../../../graphics/Icon/index.tsx'
-import { Logo as Logo_217937c36742cdefe571d11857c968fa } from '../../../graphics/Logo/index.tsx'
+import { Icon as Icon_66f12866e30467af7ec3f349143f7565 } from '../../../components/payload/Icon/index.tsx'
+import { Logo as Logo_0b25e97a21cfa283f9a68d1ebc2971b6 } from '../../../components/payload/Logo/index.tsx'
 import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
@@ -49,7 +49,7 @@ export const importMap = {
   "@payloadcms/plugin-search/client#ReindexButton": ReindexButton_aead06e4cbf6b2620c5c51c9ab283634,
   "@/Header/RowLabel#RowLabel": RowLabel_ec255a65fa6fa8d1faeb09cf35284224,
   "@/Footer/RowLabel#RowLabel": RowLabel_1f6ff6ff633e3695d348f4f3c58f1466,
-  "/graphics/Icon/index.tsx#Icon": Icon_9c1a3ff8a9adb7b1e4b7a5cd42725bdb,
-  "/graphics/Logo/index.tsx#Logo": Logo_217937c36742cdefe571d11857c968fa,
+  "/components/payload/Icon/index.tsx#Icon": Icon_66f12866e30467af7ec3f349143f7565,
+  "/components/payload/Logo/index.tsx#Logo": Logo_0b25e97a21cfa283f9a68d1ebc2971b6,
   "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
