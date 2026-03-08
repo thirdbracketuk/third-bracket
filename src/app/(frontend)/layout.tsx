65066465b77d100946ab68d8c6a3505df2bc7623
Third Bracket Ltd <hello@thirdbracket.co.uk>
2026-03-09 03:24:03 +0600

    Meta Pixel

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -139,6 +139,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <head>
         <meta name="msvalidate.01" content="46803F5EEF01F535EF3999B5E1F48682" />
+        <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1511303064341320');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1511303064341320&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
         <ThemeScript />
         <script
           type="application/ld+json"
