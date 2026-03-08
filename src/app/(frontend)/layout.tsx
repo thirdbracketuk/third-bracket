cca14aa240682df1c35e36ac7f77241c7d127578
sagarmusabbir <sagarmusabbir@gmail.com>
2026-03-09 04:00:30 +0600

    meta-pixel-fix2

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -140,20 +140,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <head>
         <meta name="msvalidate.01" content="46803F5EEF01F535EF3999B5E1F48682" />
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1511303064341320');
-    fbq('track', 'PageView');
-  `}
-        </Script>
+
         <ThemeScript />
         <script
           type="application/ld+json"
@@ -164,6 +151,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         <meta name="facebook-domain-verification" content="jvzc8wivgbd7yp2malwzexmw1rj7rh" />
       </head>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1511303064341320');
+      fbq('track', 'PageView');
+    `,
+        }}
+      />
       <body suppressHydrationWarning>
         {GTM_ID && (
           <noscript>
