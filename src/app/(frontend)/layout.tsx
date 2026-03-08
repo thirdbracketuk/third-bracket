5ccaac038fc3ed2cd0a7f2fcc6f0bd3629e6d494
sagarmusabbir <sagarmusabbir@gmail.com>
2026-03-09 03:33:13 +0600

    meta-pixel-fix

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -7,6 +7,7 @@ import { Metadata } from 'next'
 
 import GTM from '@/components/GTM'
 import { Inter, Noto_Sans_Bengali } from 'next/font/google'
+import Script from 'next/script'
 
 const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-5LLRMTFW'
 const roboto = Inter({
@@ -139,23 +140,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <head>
         <meta name="msvalidate.01" content="46803F5EEF01F535EF3999B5E1F48682" />
-        <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1511303064341320');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1511303064341320&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1511303064341320');
+    fbq('track', 'PageView');
+  `}
+        </Script>
         <ThemeScript />
         <script
           type="application/ld+json"
